3b87052da334b4ad6d522e9c5bb0599e6e4c002b
Rob Ireton <rob@ireton.org>
2018-05-20 19:35:00 -0400

    more modern

--- sunflower.js
+++ sunflower.js
@@ -3,7 +3,7 @@
 let sunflower
 
 Drupal.behaviors.sunflower = {
-  attach: function(context, settings) {
+  attach: (context, settings) => {
     const canvas       = document.getElementById('canvas')
     const controlStep  = document.getElementById('controlStep')
     const controlPause = document.getElementById('controlPause')
@@ -23,79 +23,83 @@ Drupal.behaviors.sunflower = {
     buttons.style.display = 'block'
 
     controlStep.addEventListener('change', () => {sunflower.draw()}, false)
-    controlPause.addEventListener('click', (function () {this.style.display = 'none'; controlStart.style.display = 'inline-block'; sunflower.draw();}), false)
-    controlStart.addEventListener('click', (function () {this.style.display = 'none'; controlPause.style.display = 'inline-block'; if (controlStep.value >= sunflower.limit) controlStep.value = 0; sunflower.draw();}), false)
-    controlBegin.addEventListener('click', (function () {controlStep.value = 0; sunflower.draw();}), false)
-    controlEnd.addEventListener('click', (function () {controlStep.value = sunflower.limit; sunflower.draw();}), false)
+    controlPause.addEventListener('click', function () {this.style.display = 'none'; controlStart.style.display = 'inline-block'; sunflower.draw()}, false)
+    controlStart.addEventListener('click', function () {this.style.display = 'none'; controlPause.style.display = 'inline-block'; if (controlStep.value >= sunflower.limit) controlStep.value = 0; sunflower.draw()}, false)
+    controlBegin.addEventListener('click', () => {controlStep.value = 0; sunflower.draw()}, false)
+    controlEnd.addEventListener('click', () => {controlStep.value = sunflower.limit; sunflower.draw()}, false)
 
     sunflower.draw()
   }
 }
 
-function Sunflower(radius, scale) {
+class Sunflower {
+  constructor(radius, scale) {
     this.radius = radius
     this.scale  = scale
     this.limit  = Math.floor(Math.pow(radius/scale, 2))
+    this.step   = document.getElementById('controlStep')
+    this.start  = document.getElementById('controlStart')
+    this.pause  = document.getElementById('controlPause')
+    this.canvas = document.getElementById('canvas')
+    this.ctx    = canvas.getContext('2d')
+  }
 
-    this.draw = (function () {
-        const controlStep = document.getElementById('controlStep')
-        if (controlStep.value > this.limit) {
-            controlStep.value = this.limit
-        }
-        const canvas = document.getElementById('canvas')
-        const ctx = canvas.getContext('2d')
-        ctx.save()
-        ctx.clearRect(0, 0, canvas.width, canvas.height)
-        ctx.translate(canvas.width/2, canvas.height/2)
-
-        ctx.fillStyle   = 'rgb(128, 128, 0)'
-        ctx.strokeStyle = 'rgb(64, 96, 0)'
-        ctx.lineWidth = this.scale + (this.scale / 2 * controlStep.value / this.limit)
-        ctx.beginPath()
-        ctx.arc(0, 0, this.scale * (1 + Math.sqrt(controlStep.value)), 0, 2*Math.PI, false)
-        ctx.fill()
-        ctx.stroke()
-        ctx.closePath()
-
-        ctx.strokeStyle = 'rgb(16, 64, 0)'
-        ctx.fillStyle   = 'rgb(200, 200, 0)'
-        ctx.lineCap     = 'square'
-
-        for (let seed = 0; seed <= controlStep.value; seed++) {
-            this.place(ctx, seed, controlStep.value)
-        }
-        ctx.restore()
-
-        if (parseInt(controlStep.value, 10) < parseInt(controlStep.max, 10)) {
-            if (document.getElementById('controlStart').style.display == 'none') {
-                controlStep.value++
-                setTimeout((function () {sunflower.draw()}), 20)
-            }
-        }
-        else {
-            document.getElementById('controlPause').style.display = 'none'
-            document.getElementById('controlStart').style.display = 'inline-block'
-        }
-
-        return true
-    })
-
-    this.place = function (context, seed, frame) {
-        context.save()
-        const φ_radians = Math.PI * (1 + Math.sqrt(5))
-        const ρ    = this.scale * Math.sqrt(frame-seed)
-        const size = this.scale + (this.scale/2 * (frame-seed) / this.limit)
-
-        context.lineWidth = size/3
-        context.rotate(seed * φ_radians)
-        context.beginPath()
-        context.moveTo(ρ-size/2,        0)
-        context.lineTo(ρ,          size/2)
-        context.lineTo(ρ+size/2,        0)
-        context.lineTo(ρ,         -size/2)
-        context.lineTo(ρ-size/2,        0)
-        context.fill()
-        context.stroke()
-        context.restore()
+  draw() {
+    if (this.step.value > this.limit) {
+        this.step.value = this.limit
     }
+
+    this.ctx.save()
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.translate(this.canvas.width/2, this.canvas.height/2)
+
+    this.ctx.fillStyle   = 'rgb(128, 128, 0)'
+    this.ctx.strokeStyle = 'rgb(64, 96, 0)'
+    this.ctx.lineWidth = this.scale + (this.scale / 2 * this.step.value / this.limit)
+    this.ctx.beginPath()
+    this.ctx.arc(0, 0, this.scale * (1 + Math.sqrt(this.step.value)), 0, 2*Math.PI, false)
+    this.ctx.fill()
+    this.ctx.stroke()
+    this.ctx.closePath()
+
+    this.ctx.strokeStyle = 'rgb(16, 64, 0)'
+    this.ctx.fillStyle   = 'rgb(200, 200, 0)'
+    this.ctx.lineCap     = 'square'
+
+    for (let seed = 0; seed <= this.step.value; seed++) {
+        this.place(seed, this.step.value)
+    }
+    this.ctx.restore()
+
+    if (parseInt(this.step.value) < parseInt(this.step.max)) {
+        if (this.start.style.display == 'none') {
+            this.step.value++
+            setTimeout( () => sunflower.draw(), 20)
+        }
+    }
+    else {
+        this.pause.style.display = 'none'
+        this.start.style.display = 'inline-block'
+    }
+  }
+
+  place(seed, frame) {
+      this.ctx.save()
+      const φ_radians = Math.PI * (1 + Math.sqrt(5))
+      const ρ    = this.scale * Math.sqrt(frame-seed)
+      const size = this.scale + (this.scale/2 * (frame-seed) / this.limit)
+
+      this.ctx.lineWidth = size/3
+      this.ctx.rotate(seed * φ_radians)
+      this.ctx.beginPath()
+      this.ctx.moveTo(ρ-size/2,        0)
+      this.ctx.lineTo(ρ,          size/2)
+      this.ctx.lineTo(ρ+size/2,        0)
+      this.ctx.lineTo(ρ,         -size/2)
+      this.ctx.lineTo(ρ-size/2,        0)
+      this.ctx.fill()
+      this.ctx.stroke()
+      this.ctx.restore()
+  }
+
 }
